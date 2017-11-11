3e9aac010cd7d89560ba164e3a7dd6bb2dd0900d
DallonF <dallon.feldner@gmail.com>
2017-11-11 11:55:10 -0600

    Chicken out, don't include the Explanation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import Explanation from './Explanation';
 import VotesPage from './pages/Votes/VotesPage';
 import { SvgPatterns } from './styles';
 
 class App extends Component {
   render() {
-    return [
-      <Explanation key={0} />,
-      <VotesPage key={1} />,
-      <SvgPatterns key={2} />,
-    ];
+    return [<VotesPage key={1} />, <SvgPatterns key={2} />];
   }
 }
 
