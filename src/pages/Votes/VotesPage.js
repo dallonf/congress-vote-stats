205be8dc606aef3e017e1aa46118a63eabe89204
DallonF <dallon.feldner@gmail.com>
2017-11-11 11:33:17 -0600

    React Router

--- src/pages/Votes/VotesPage.js
+++ src/pages/Votes/VotesPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import moment from 'moment';
+import qs from 'query-string';
+import { Route, withRouter } from 'react-router';
 import Votes from './Votes';
 
 const START_DATE = '1991-01-01';
@@ -15,10 +17,19 @@ const OptionHeader = styled.div`
 `;
 
 class VotePage extends React.Component {
-  state = { month: moment().get('month'), year: moment().get('year') };
+  // state = { month: moment().get('month'), year: moment().get('year') };
 
   render() {
-    const { month, year } = this.state;
+    const { location, history } = this.props;
+    const params = qs.parse(location.search);
+    let month = parseInt(params.month, 10) - 1;
+    if (typeof month !== 'number' || Number.isNaN(month)) {
+      month = moment().get('month');
+    }
+    let year = parseInt(params.year, 10);
+    if (typeof year !== 'number' || Number.isNaN(year)) {
+      year = moment().get('year');
+    }
 
     const options = [];
     let cursor = moment().startOf('month');
@@ -41,9 +52,12 @@ class VotePage extends React.Component {
             onChange={e => {
               const newSelection = e.target.value;
               const selectionMoment = moment(newSelection, 'YYYY-MM');
-              this.setState({
-                month: selectionMoment.get('month'),
-                year: selectionMoment.get('year'),
+              history.push({
+                ...location,
+                search: qs.stringify({
+                  month: selectionMoment.get('month') + 1,
+                  year: selectionMoment.get('year'),
+                }),
               });
             }}
           >
@@ -66,4 +80,4 @@ class VotePage extends React.Component {
   }
 }
 
-export default VotePage;
+export default withRouter(VotePage);
