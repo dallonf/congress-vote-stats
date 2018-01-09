74f324fb761cceb5ba1abe4d46d659cbb516b00e
DallonF <dallon.feldner@gmail.com>
2018-01-08 20:30:55 -0600

    Figured out how Serverless works

--- backend/handler.js
+++ backend/handler.js
@@ -9,9 +9,17 @@ module.exports.hello = (event, context, callback) => {
     }),
   };
 
-  console.log('wat');
   callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.getMonthStats = (event, context, callback) => {
+  const { month, year } = event.pathParameters;
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ month, year }),
+  };
+  callback(null, response);
 };
